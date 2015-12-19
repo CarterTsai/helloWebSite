647c678e8f65bff5b40f992f1a786a8a7d755007
CarterTsai <hamming1@gmail.com>
2015-12-19 19:49:21 +0800

    Update

--- app/script/datas.js
+++ app/script/datas.js
@@ -10,6 +10,38 @@ var Data = [{
     'url':'https://www.chung-yo.com/Product/Division/2',
     'name': '服飾/配件',
     'imageName': 'Division2.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/3',
+    'name': '居家',
+    'imageName': 'Division3.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/4',
+    'name': '戶外旅遊',
+    'imageName': 'Division4.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/5',
+    'name': '婦幼親子',
+    'imageName': 'Division5.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/6',
+    'name': '美妝',
+    'imageName': 'Division6.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/7',
+    'name': '3C家電',
+    'imageName': 'Division7.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/8',
+    'name': '國際精品',
+    'imageName': 'Division8.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/9',
+    'name': '運動',
+    'imageName': 'Division9.png'
+},{
+    'url':'https://www.chung-yo.com/Product/Division/10',
+    'name': '百貨專區',
+    'imageName': 'Division10.png'
 }];
 
 var exports = module.exports = Data;
